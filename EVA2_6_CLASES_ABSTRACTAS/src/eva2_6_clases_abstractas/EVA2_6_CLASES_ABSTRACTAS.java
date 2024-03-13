4a8c898d925d7e65d5e715b99b46793cd5868263
Luisrivas29 <143302710+Luisrivas29@users.noreply.github.com>
2024-03-12 22:41:36 -0600

    Add files via upload

---
+++ EVA2_6_CLASES_ABSTRACTAS/src/eva2_6_clases_abstractas/EVA2_6_CLASES_ABSTRACTAS.java
@@ -0,0 +1,12 @@
+package eva2_6_clases_abstractas;
+
+
+public class EVA2_6_CLASES_ABSTRACTAS {
+
+   
+    public static void main(String[] args) {
+        Triangulo triangulo = new Triangulo();
+        //Figura figu = new Figura(); -- no podemos crear objetos de clases abstractas
+    }
+    
+}
---
+++ EVA2_6_CLASES_ABSTRACTAS/src/eva2_6_clases_abstractas/Figura.java
@@ -0,0 +1,9 @@
+
+package eva2_6_clases_abstractas;
+
+
+public abstract class Figura {
+    public abstract double calcularArea();
+    public abstract double calcularPeri();
+    
+}
